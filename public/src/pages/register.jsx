21178b608d23997c1bd10202342435acde846c47
ngxuantai <xuantai1902@gmail.com>
2023-08-14 17:42:13 +0700

    create user function FE

--- public/src/pages/register.jsx
+++ public/src/pages/register.jsx
@@ -1,55 +1,107 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {Link, Navigate, useNavigate} from 'react-router-dom';
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import styled from 'styled-components';
 import logo from '../assets/logo.png';
+import {createUser} from '../services/userApi';
 
 function Register() {
-  const handleSubmit = (event) => {};
+  const navigate = useNavigate();
 
-  const handleChange = (event) => {};
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const toastOptions = {
+    position: 'top-right',
+    autoClose: 3000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: 'dark',
+  };
+
+  const handleValidation = () => {
+    const {username, email, password, confirmPassword} = values;
+    if (username.length < 3) {
+      toast.error('Username must be at least 3 characters.', toastOptions);
+      return false;
+    } else if (email === '') {
+      toast.error('Email is required.', toastOptions);
+      return false;
+    } else if (password !== confirmPassword) {
+      toast.error('Password and confirm password do not match.', toastOptions);
+      return false;
+    } else if (password.length < 8) {
+      toast.error('Password must be ay least 8 characters.', toastOptions);
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      const {username, email, password} = values;
+      const {data} = await createUser({username, email, password});
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
+      } else {
+        navigate('/');
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setValues({...values, [name]: value});
+  };
 
   return (
     <>
       <FormContainer>
         <form onSubmit={(event) => handleSubmit(event)}>
-          <div className='logo'>
-            <img src={logo} alt='logo' />
+          <div className="logo">
+            <img src={logo} alt="logo" />
             <h1>Chat App</h1>
           </div>
           <input
-            autoComplete='off'
-            type='text'
-            name='username'
-            placeholder='Username'
+            autoComplete="off"
+            type="text"
+            name="username"
+            placeholder="Username"
             onChange={(event) => handleChange(event)}
           />
           <input
-            autoComplete='off'
-            type='email'
-            name='email'
-            placeholder='Email'
+            autoComplete="off"
+            type="email"
+            name="email"
+            placeholder="Email"
             onChange={(event) => handleChange(event)}
           />
           <input
-            autoComplete='off'
-            type='password'
-            name='password'
-            placeholder='Password'
+            autoComplete="off"
+            type="password"
+            name="password"
+            placeholder="Password"
             onChange={(event) => handleChange(event)}
           />
           <input
-            autoComplete='off'
-            type='password'
-            name='confirmPassword'
-            placeholder='Confirm Password'
+            autoComplete="off"
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
             onChange={(event) => handleChange(event)}
           />
-          <button type='submit'>Create account</button>
+          <button type="submit">Create account</button>
           <span>
-            Already have a account? <Link to='/login'>Login</Link>
+            Already have a account? <Link to="/login">Login</Link>
           </span>
         </form>
       </FormContainer>
+      <ToastContainer />
     </>
   );
 }
